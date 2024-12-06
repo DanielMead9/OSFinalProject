d93c221ca7a0797f5071d74c3aa73f95361947ab
Daniel <danielmead@Daniels-MacBook-Air-3.local>
2024-12-05 23:04:35 -0500

    Bug Fix

--- Edited Thread Creator.c
+++ Edited Thread Creator.c
@@ -12,8 +12,7 @@ typedef struct {
     int resource_request; // Added Characteristic based on Professor request
 } ThreadInfo;
 
-int thread_count, resource_count, deadlock = 0;
-int allocated[resource_count];
+int thread_count = 0, resource_count = 0, deadlock = 0;
 
 void *use_resources(void *arg) {
     ThreadInfo *info = (ThreadInfo *)arg;
@@ -28,7 +27,8 @@ void *use_resources(void *arg) {
     pthread_exit(NULL);
 }
 
-void detector(ThreadInfo thread_info[]) {
+void detector(ThreadInfo thread_info[], int allocated[]) {
+
     for (int i = 0; i < thread_count; i++) {
         for (int j = 0; j < thread_info[i].resource_count; j++){
             allocated[thread_info[i].resources[j]]++;
@@ -43,8 +43,9 @@ void detector(ThreadInfo thread_info[]) {
     }
 }
 
-void requestdetector(ThreadInfo thread_info[]) {
+void requestdetector(ThreadInfo thread_info[], int allocated[]) {
     int requested[resource_count];
+
     for (int i = 0; i < thread_count; i++) {
         for (int j = 0; j < thread_info[i].resource_count; j++){
             requested[thread_info[i].resource_request]++;
@@ -53,10 +54,10 @@ void requestdetector(ThreadInfo thread_info[]) {
     for (int i = 0; i < sizeof(requested); i++){
         if (requested[i] > 0 && allocated[i] > 1){
             for (int k = 0; k < thread_count; k++){
-                for (int j = 0; j < thread_info[k]; j++){
+                for (int j = 0; j < thread_info[k].resource_count; j++){
                     if (thread_info[k].resources[j] == i && allocated[thread_info[k].resource_request] > 0){
                         deadlock = 1;
-
+                        printf("Pizza");
                     }
                 }
             }
@@ -83,6 +84,7 @@ int main() {
 
     pthread_t threads[thread_count];
     ThreadInfo thread_info[thread_count];
+    int allocated[resource_count];
 
     /*
         Input resource allocation for each thread, handles numeric input separated by commas
@@ -125,7 +127,7 @@ int main() {
         on after to resource requests per thread.
     ======================================================================================
     */
-   detector(thread_info);
+   detector(thread_info, allocated);
     /*
         User input for singular resource request for each thread
 
@@ -144,6 +146,8 @@ int main() {
             thread_info[i].resource_request = -1;
         }
     }
+
+    requestdetector(thread_info, allocated);
 
     // Create threads
     for (int i = 0; i < thread_count; i++) {
